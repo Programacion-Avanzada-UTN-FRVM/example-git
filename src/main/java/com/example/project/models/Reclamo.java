44383ce3e02b79944fadea4a94c79b110f3f240f
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-10-22 16:21:57 -0300

    feat: crear modelos reclamo y tipoReclamo

--- src/main/java/com/example/project/models/Reclamo.java
+++ src/main/java/com/example/project/models/Reclamo.java
@@ -1,6 +1,6 @@
 package com.example.project.models;
 
-import java.time.LocalDateTime;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,11 +8,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
+
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Entity
+@Builder
 public class Reclamo {
     
     @Id
@@ -25,22 +27,18 @@ public class Reclamo {
 
     private String descripcion;
 
-    private boolean esCritico;
-
-    private LocalDateTime creationDatetime;
-
-    @PrePersist //Este metodo se ejecutara antes de crear persistir al objeto en la BD
-    public void preCreate() {
-        creationDatetime = LocalDateTime.now();
-    }
+    private boolean critico;
 
     public Reclamo() {
 
     }
 
-    public Reclamo(TipoReclamo tipoReclamo, String descripcion, boolean esCritico) {
+    public Reclamo(Long id, TipoReclamo tipoReclamo, String descripcion, boolean critico) {
+        this.id = id;
         this.tipoReclamo = tipoReclamo;
         this.descripcion = descripcion;
-        this.esCritico = esCritico;
+        this.critico = critico;
     }
+
+    
 }
